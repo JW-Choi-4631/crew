f82a357d864a9a5c1c9d32cbedec71c5fec2dc75
최정운 <choijung4631@naver.com>
2023-08-13 16:41:49 +0900

    네이버 맵 연결

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -11,7 +11,7 @@ function Main() {
       <div style={{ display: "flex", flexDirection: "column", width: "375px", height: "812px", backgroundColor: "white", padding: "16px" }}>
         <div style={{ width: "100%", height: "112px", backgroundColor: "#ECEFF2", borderRadius: "22px" }}></div>
         <div style={{ display: "flex", width: "100%", height: "204px", marginTop: "17px", gap: "9px" }}>
-          <div style={{ width: "50%", height: "100%", backgroundColor: "#ECEFF2", borderRadius: "22px", cursor: "pointer" }}>
+          <div onClick={() => navigate("/find_crew")} style={{ width: "50%", height: "100%", backgroundColor: "#ECEFF2", borderRadius: "22px", cursor: "pointer" }}>
             <p style={{ fontWeight: "bold", marginLeft: "10px" }}>내 주변 모임 찾기</p>
           </div>
           <div onClick={() => navigate("/create_crew")} style={{ width: "50%", height: "100%", backgroundColor: "#ECEFF2", borderRadius: "22px", cursor: "pointer" }}>
